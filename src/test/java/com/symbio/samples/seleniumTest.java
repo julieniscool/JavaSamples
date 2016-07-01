ca44ba7a5bc475bf9e2a85ce5c00a4b7adfacc27
Julien Vely <jvely@paypal.com>
2016-07-01 19:02:49 +0800

    first commit

---
+++ src/test/java/com/symbio/samples/seleniumTest.java
@@ -0,0 +1,42 @@
+package com.symbio.samples;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Unit test for simple App.
+ */
+public class seleniumTest
+{
+
+    @Test
+    public void Method1() throws InterruptedException, IOException {
+
+        WebDriver driver = new FirefoxDriver();
+
+        // A "base url", used by selenium to resolve relative URLs
+        driver.get("http://www.google.com");
+
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.name("q"));
+
+        // Enter something to search for
+        element.sendKeys("Cheese!");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+
+        // Should see: "cheese! - Google Search"
+        System.out.println("Page title is: " + driver.getTitle());
+
+        //Close the browser
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/symbio/samples/testngTest.java
@@ -0,0 +1,37 @@
+package com.symbio.samples;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class testngTest
+{
+
+    @Test
+    public void method1() {
+        long id = Thread.currentThread().getId();
+        System.out.println("method1 is executed in thread " + id);    
+    }
+
+   @Test    
+    public void method2() {
+        long id = Thread.currentThread().getId();
+        System.out.println("method2 is executed in thread " + id);    
+    }
+
+   @Test    
+    public void method3() {
+        long id = Thread.currentThread().getId();
+        System.out.println("method3 is executed in thread " + id);
+    }
+
+ 
+}
